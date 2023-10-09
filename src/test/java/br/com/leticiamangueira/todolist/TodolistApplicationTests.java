b9541c4465ece2a7525e534219d7b66448cc272a
leticea <nascimento.leticeah@gmail.com>
2023-10-09 20:17:37 -0300

    first commit: creating routes

---
+++ src/test/java/br/com/leticiamangueira/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leticiamangueira.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
