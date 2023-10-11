05e608fed5ce0cb1eaa30327b571df225d63bff7
leticea <nascimento.leticeah@gmail.com>
2023-10-11 19:31:35 -0300

    feat(UserController): creating hashred password

--- src/main/java/br/com/leticiamangueira/todolist/user/UserController.java
+++ src/main/java/br/com/leticiamangueira/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -23,6 +25,9 @@ public class UserController {
       System.out.println("Usuário já existe!");
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
     }
+
+    var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+    userModel.setPassword(passwordHashred);
 
     var userCreated = this.userRepository.save(userModel);
     return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
