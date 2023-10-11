f8dd96ee396d88fbcae1c77fd074d762138af47d
leticea <nascimento.leticeah@gmail.com>
2023-10-11 20:47:30 -0300

    feat(FilterTaskAuth): getting authorization - username and password

--- src/main/java/br/com/leticiamangueira/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/leticiamangueira/todolist/filter/FilterTaskAuth.java
@@ -3,22 +3,34 @@ package br.com.leticiamangueira.todolist.filter;
 import java.io.IOException;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+
 
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
 
-    System.out.println("Chegou no filtro");
-    chain.doFilter(request, response);
+        // Pegar a autenticação (usuário e senha)
+        var authorization = request.getHeader("Authorization");
+        System.out.println("Authorization");
+        System.out.println(authorization);
+
+
+        // Validar usuário
+
+        // Validar senha
+
+        // Segue viagem
+
+        filterChain.doFilter(request, response);
   }
-
 }
