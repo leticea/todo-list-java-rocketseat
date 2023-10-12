85c9c93915ef100c2a8f5677eca222997214adfb
leticea <nascimento.leticeah@gmail.com>
2023-10-12 13:37:31 -0300

    feat(TaskController): creating startAt and endAt

--- src/main/java/br/com/leticiamangueira/todolist/task/TaskController.java
+++ src/main/java/br/com/leticiamangueira/todolist/task/TaskController.java
@@ -1,8 +1,11 @@
 package br.com.leticiamangueira.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +21,20 @@ public class TaskController {
   private ITaskRepository taskRepository;
 
   @PostMapping("/")
-  public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+  public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
     var idUser = request.getAttribute("idUser");
     taskModel.setIdUser((UUID) idUser);
+
+    var currentDate = LocalDateTime.now();
+
+    if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início / data de término deve ser maior do que a data atual");
+    }
+
+
     var task = this.taskRepository.save(taskModel);
-    return task;
+    return ResponseEntity.status(HttpStatus.OK).body(task);
   }
 }
+
+// PAREI NO MINUTO -34
