e48eb9f2d40b2c8cdf58511e7666460480f03f73
leticea <nascimento.leticeah@gmail.com>
2023-10-11 22:22:32 -0300

    feat(FilterTaskAuth): checking if the password is correct and the user exists

--- src/main/java/br/com/leticiamangueira/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/leticiamangueira/todolist/filter/FilterTaskAuth.java
@@ -27,8 +27,18 @@ public class FilterTaskAuth extends OncePerRequestFilter {
 
         byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
 
+        var authString = new String(authDecoded);
+
+
+
+        String[] credentials = authString.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
+
         System.out.println("Authorization");
-        System.out.println(authDecoded);
+        System.out.println(username);
+        System.out.println(password);
+
 
 
         // Validar usuário
